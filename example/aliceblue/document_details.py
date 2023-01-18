37954cb239a9fbb3436704efe31a762fd68b95a3
SRMSOFTDEV <119946630+SRMSOFTDEV@users.noreply.github.com>
2023-01-18 11:40:13 +0530

    Create document_details.py

---
+++ example/aliceblue/document_details.py
@@ -0,0 +1,7 @@
+email_id ='abcdegmail.com'
+email_password = 'xxxxxxxxxxxxxxxx'
+username = 'alcieblie_id'
+api_key = 'apikey'
+
+
+#TVALERT#{'strategy':'Supertrend_10_2','exchange':'{{exchange}}','ticker':'{{ticker}}','price':{{close}},'action':'{{strategy.order.action}}','quantity':'{{strategy.position_size}}'}#TVALERT#
